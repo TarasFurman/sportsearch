23f2ad2646f9a7f3b73c236d202a5732d56d64bf
olegmikadze <olegmikadze10@gmail.com>
2019-03-26 17:45:10 +0200

    hotfixes

--- frontend/src/pages/ProfilePage/profile/ProfileView.jsx
+++ frontend/src/pages/ProfilePage/profile/ProfileView.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import {Col,Row,Container,Image} from "react-bootstrap";
-import "../../../style/profileStyles/css/style.css";
-import imgProfile from "../../../style/profileStyles/img/img-profile.jpg";
-import userRating from "../../../style/profileStyles/img/rating-background.svg";
+import "../profileStyles/css/style.css";
+import imgProfile from "../profileStyles/img/img-profile.jpg";
+import userRating from "../profileStyles/img/rating-background.svg";
 import { Fa,FaViber, FaTelegram, FaRegEdit } from 'react-icons/fa';
 
 
