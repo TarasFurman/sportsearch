cf49307b80d936fea8b068e63cf4acc51d5d5467
olegmikadze <olegmikadze10@gmail.com>
2019-03-28 17:45:36 +0200

    settings styles

--- frontend/src/pages/SettingsPage/Settings.jsx
+++ frontend/src/pages/SettingsPage/Settings.jsx
@@ -67,93 +67,139 @@ class Settings extends React.Component{
             <div className="settingsWrapper">
                 <form className="settingsForm" onSubmit={this.handleSubmit}>
                     <h1>Notifications could be sent to:</h1>
-                    Email
-                    <input
-                        name="emailNotification"
-                        type="checkbox"
-                        checked={this.state.emailNotification}
-                        onChange={this.handleInputChange}
-                    />
-                    Telegram
-                    <input
-                        name="telegramNotification"
-                        type="checkbox"
-                        checked={this.state.telegramNotification}
-                        onChange={this.handleInputChange}
-                    />
-                    Viber
-                    <input
-                        name="viberNotification"
-                        type="checkbox"
-                        checked={this.state.viberNotification}
-                        onChange={this.handleInputChange}
-                    />
+                    <div className="notificationReceiver">
+                        <div>
+                            <span>Email</span>
+                            <input
+                                name="emailNotification"
+                                type="checkbox"
+                                checked={this.state.emailNotification}
+                                onChange={this.handleInputChange}
+                                className="notificationInput"
+                            />
+                        </div>
+                        
+                        <div>
+                            <span>Telegram</span>
+                            <input
+                                name="telegramNotification"
+                                type="checkbox"
+                                checked={this.state.telegramNotification}
+                                onChange={this.handleInputChange}
+                                className="notificationInput"
 
+                            />
+                        </div>
+                        
+                        <div>
+                            <span>Viber</span>
+                            <input
+                                name="viberNotification"
+                                type="checkbox"
+                                checked={this.state.viberNotification}
+                                onChange={this.handleInputChange}
+                                className="notificationInput"
+
+                            />
+                        </div>
+                    </div>
+                    
                     <h1>Type notification:</h1>
-                    Your request has been approved
-                    <input
-                        name="requestApproved"
-                        type="checkbox"
-                        checked={this.state.requestApproved}
-                        onChange={this.handleInputChange}
-                    />
-                    Your request has been rejected
-                    <input
-                        name="requestRejected"
-                        type="checkbox"
-                        checked={this.state.requestRejected}
-                        onChange={this.handleInputChange}
-                    />
-                    You have been kicked from event
-                    <input
-                        name="isKicked"
-                        type="checkbox"
-                        checked={this.state.isKicked}
-                        onChange={this.handleInputChange}
-                    />
-                    Event finished
-                    <input
-                        name="eventFinished"
-                        type="checkbox"
-                        checked={this.state.eventFinished}
-                        onChange={this.handleInputChange}
-                    />
-                    Event canceled
-                    <input
-                        name="eventCanceled"
-                        type="checkbox"
-                        checked={this.state.eventCanceled}
-                        onChange={this.handleInputChange}
-                    />
-                    You received feedback
-                    <input
-                        name="receivedFeedback"
-                        type="checkbox"
-                        checked={this.state.receivedFeedback}
-                        onChange={this.handleInputChange}
-                    />
-                    1 hour before the event
-                    <input
-                        name="beforeEvent"
-                        type="checkbox"
-                        checked={this.state.beforeEvent}
-                        onChange={this.handleInputChange}
-                    />
-                    When new request appears (for event owners only)
-                    <input
-                        name="eventRequest"
-                        type="checkbox"
-                        checked={this.state.eventRequest}
-                        onChange={this.handleInputChange}
-                    />
-                    You were invited to the event 
-                    <input
-                        name="eventInvitation"
-                        type="checkbox"
-                        checked={this.state.eventInvitation}
-                        onChange={this.handleInputChange}
-                    />
-                    <button type="submit">Submit</button>
+
+                    <div className="notificationTypeDiv">
+                       
+                        <div className="notificationTexts">
+                            <span> <input
+                                name="requestApproved"
+                                type="checkbox"
+                                checked={this.state.requestApproved}
+                                onChange={this.handleInputChange}
+                                className="notificationInput"
+                            />
+                            Your request has been approved</span>
+                            <span>
+                                <input
+                                    name="requestRejected"
+                                    type="checkbox"
+                                    checked={this.state.requestRejected}
+                                    onChange={this.handleInputChange}
+                                    className="notificationInput"
+                                />
+                                Your request has been rejected
+                            </span>
+                            <span>
+                                <input
+                                    name="isKicked"
+                                    type="checkbox"
+                                    checked={this.state.isKicked}
+                                    onChange={this.handleInputChange}
+                                    className="notificationInput"
+                                />
+                                You have been kicked from event
+                            </span>
+                            <span>
+                                <input
+                                    name="eventFinished"
+                                    type="checkbox"
+                                    checked={this.state.eventFinished}
+                                    onChange={this.handleInputChange}
+                                    className="notificationInput"
+                                />
+                                Event finished
+                            </span>
+                            <span>
+                                <input
+                                    name="eventCanceled"
+                                    type="checkbox"
+                                    checked={this.state.eventCanceled}
+                                    onChange={this.handleInputChange}
+                                    className="notificationInput"
+                                />
+                                Event canceled
+                            </span>
+                            <span>
+                                <input
+                                    name="receivedFeedback"
+                                    type="checkbox"
+                                    checked={this.state.receivedFeedback}
+                                    onChange={this.handleInputChange}
+                                    className="notificationInput"
+                                />
+                                You received feedback
+                            </span>
+                            <span>
+                                <input
+                                    name="beforeEvent"
+                                    type="checkbox"
+                                    checked={this.state.beforeEvent}
+                                    onChange={this.handleInputChange}
+                                    className="notificationInput"
+                                />
+                                1 hour before the event
+                            </span>
+                            <span>
+                                <input
+                                    name="eventRequest"
+                                    type="checkbox"
+                                    checked={this.state.eventRequest}
+                                    onChange={this.handleInputChange}
+                                    className="notificationInput"
+                                />
+                            When new request appears (for event owners only)</span>
+                            <span>
+                                <input
+                                    name="eventInvitation"
+                                    type="checkbox"
+                                    checked={this.state.eventInvitation}
+                                    onChange={this.handleInputChange}
+                                    className="notificationInput"
+                                />
+                            You were invited to the event </span>
+                        </div>                    
+                    </div>
+
+                    
+                    <button className="submitSettings" type="submit">Submit</button>
                 </form>
             </div>
         )
