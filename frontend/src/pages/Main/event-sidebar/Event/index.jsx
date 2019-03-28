2bbaf231ad469102a8ce4cd33c4f8d40372ff66d
olegmikadze <olegmikadze10@gmail.com>
2019-03-28 16:37:55 +0200

    hotfix styles events

--- frontend/src/pages/Main/event-sidebar/Event/index.jsx
+++ frontend/src/pages/Main/event-sidebar/Event/index.jsx
@@ -46,24 +46,20 @@ class Event extends React.Component {
 
     if (this.state.isAllInfo) {
       fullDescription = (
-        <div className="fullDescription">
-          <FaInfoCircle />
-          <span className="">{this.props.description}</span>
+        <div className="fullDescription partDescription">
+          <span className=""><FaInfoCircle />{' '}{this.props.description}</span>
         </div>
       );
       fullOwner = (
-        <div className="fullOwner">
-          <FaUserCircle />
+        <div className="fullOwner partDescription">
+        <FaUserCircle />
           <AnotherUserProfileCard userId={this.props.owner_id}>
-            <span className="fullOwnerSpan">{this.props.owner}</span>
+            <span className="fullOwnerSpan ">{' '}{this.props.owner}</span>
           </AnotherUserProfileCard>
         </div>
       );
       fullRating = (
-        <div className="fullRating">
-          <FaStar />
-          <span className="fullRatingSpan">{this.props.owner_rating}</span>
-        </div>
+        <span className="fullRatingSpan partDescription"><FaStar />{' '}{this.props.owner_rating}</span>
       );
 
       buttonDownUp = <FaAngleUp className="arrowBtn" onClick={this.handleClick} />;
@@ -74,45 +70,28 @@ class Event extends React.Component {
       <IconContext.Provider value={{ className: 'global-svg-icon-class' }}>
         <div className="eventWrapper">
           <div className="eventName">
-            <h4 className="eventTitle">{this.props.name}</h4>
+            <h2 className="eventTitle">{this.props.name}</h2>
           </div>
           <div className="mainEventInfo">
             <Image src={this.props.img_url} thumbnail className="d-block" />
             <div className="rightPart">
-              <span className="center">
-                {' '}
-                <GiAmericanFootballBall />
-                {' '}
-                {this.props.sport_type}
-              </span>
               <div className="center">
-                <FaDollarSign />
-                {' '}
-                <span className="priceSpan">{this.props.price}</span>
+                <span><GiAmericanFootballBall /></span>
+                <span><FaDollarSign /></span>
+                <span><IoIosPeople /></span>
               </div>
               <div className="center">
-                <IoIosPeople />
-                {' '}
-                <span className="membersSpan">
-                  {this.props.members}
-                  {' '}
-                   of
-                  {' '}
-                  {this.props.members_total}
-                </span>
+                <span className="priceSpan">{this.props.sport_type}</span>
+                <span className="priceSpan">{this.props.price}</span>
+                <span className="membersSpan">{this.props.members}{' '}of{' '}{this.props.members_total}</span>
               </div>
             </div>
           </div>
-          <div className="center">
-            <FaClock />
-            {' '}
-            <span className="dateSpan">{this.props.datetime}</span>
+          <div className="partDescription">
+            <span className="dateSpan"><FaClock />{' '}{this.props.datetime}</span>
           </div>
-          <div className="center">
-            <MdLocationOn />
-            
-            {' '}
-            <span className="adressSpan">{this.props.adress}</span>
+          <div className="partDescription">
+            <span className="addressSpan"><MdLocationOn />{' '}{this.props.adress}</span>
           </div>
           {fullOwner}
           {fullRating}
