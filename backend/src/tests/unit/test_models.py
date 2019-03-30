1580b5ddf5c8ceda48702f8ee6293df8f951680b
olegmikadze <olegmikadze10@gmail.com>
2019-03-30 21:33:48 +0200

    working user model unit test

---
+++ backend/src/tests/unit/test_models.py
@@ -0,0 +1,18 @@
+import pytest
+# def capital_case(x):
+#     return x.capitalize()
+
+# def test_capital_case():
+#     assert capital_case('semaphore') == 'Semaphore'
+
+def test_new_user(new_user):
+ assert new_user.id == 330
+ assert new_user.nickname == 'nickname'
+ assert new_user.email == 'olegmikadze10@gmail.com'
+ assert new_user.phone == "+380990125386"
+ assert new_user.password == 'awesomePAssword'
+ assert new_user.first_name == 'Oleg'
+ assert new_user.last_name == 'Mikadze'
+ assert new_user.access_token == '43567ytg56g54f948f85794d'
+ assert new_user.auth_type == 'google'
+
